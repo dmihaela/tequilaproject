67788553dbbe2f81d8d72dbb27b64ef9fbb0f953
RazvanAndrei <razvanandrei@ubuntu.(none)>
2013-12-14 11:52:38 +0200

    Creating the folder application #1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tequilaproject::Application.config.secret_key_base = 'c06902de9c066d1838a702d64d2e33084ed43f9810bdcdd7c94e8b2dc55987bb8808588091d023a404695008343dcbce9b572e1b2d2086dd92e21e337350fcd1'
