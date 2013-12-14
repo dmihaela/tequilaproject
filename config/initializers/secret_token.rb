810a248e2caec0bfc4199895e411da60df2e41bb
Tudor <tudor@ubuntu.(none)>
2013-12-14 11:52:42 +0200

    #1 create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tequilaproject::Application.config.secret_key_base = '3ebf88169cdd51062677fac38aef5e0b4a3e8083d61f9138194d30f8025a6cda0d7b516d48d08be862f62036f5f3139c49010649768d89c3468d8ca54c0811ab'
